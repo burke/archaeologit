4445b0cfdbefaac0b211e67a7defd68363e9cb39
Burke Libbey <burke@burkelibbey.org>
2010-03-02 19:57:54 -0600

    added indexes

--- app.rb
+++ app.rb
@@ -10,6 +10,9 @@ get '*' do
   if File.file?(full_path)
     log = `cd #{PATH}; git log -p #{full_path}`
     haml :log, :locals => { :log => log }
+  elsif File.directory?(full_path)
+    entries = Dir.entries(full_path).reject{|e|e=~/^\./}
+    haml :index, :locals => { :entries => entries }
   else 
     "INCORRECT PATH"
   end
