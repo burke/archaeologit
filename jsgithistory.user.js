a3b1b05cf62cdfb5861dda3816f0ee577fbd0d56
Burke Libbey <burke@burkelibbey.org>
2011-01-15 23:22:30 -0600

    Made CSS marginally less horrible. Lots to be done yet.

--- jsgithistory.user.js
+++ jsgithistory.user.js
@@ -7,7 +7,6 @@
 // @include       http://github.com/*/blob/*
 // ==/UserScript==
 
-// a function that loads jQuery and calls a callback function when jQuery has finished loading
 function addJQuery(callback) {
   var script = document.createElement("script");
   script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
@@ -19,7 +18,6 @@ function addJQuery(callback) {
   document.body.appendChild(script);
 }
 
-// the guts of this userscript
 function main() {
   $(".file .actions").prepend('<li><a class="jsgithistory-link" href="#">jsgithistory</a></li>');
   $(".jsgithistory-link").click(function() {
@@ -29,5 +27,4 @@ function main() {
   });
 }
 
-// load jQuery and execute the main function
 addJQuery(main);
